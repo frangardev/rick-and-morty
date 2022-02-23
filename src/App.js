93bdef62ae2dd87657e3c840574e153de89c66bf
Francisco García <francoffee57@gmail.com>
2022-02-22 21:13:15 -0600

    Mejore la composición de componentes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,88 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Character } from './components/Character';
+import { CharacterItem } from './components/CharacterItem';
 
 //Con esto sabemos si el usuario usa darkMode o lighMode
 const isDarkUser = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
 
 function App() {  
   const [darkMode, setDarkMode] = React.useState(isDarkUser)
+  const [state, dispach] = React.useReducer(favoriteReducer, initialState)
+    
+  React.useEffect(()=>{
+      fetch('https://rickandmortyapi.com/api/character/')
+      .then(response => response.json())
+      .then(data => dispach({ type: 'CHARACTERS', payload: data.results }))
+  },[])
 
   React.useEffect(()=>{
-    const body = document.querySelector('body')
-    
+    const body = document.querySelector('body') 
     body.className = darkMode ? 'dark' : ''
   },[darkMode])
 
+  const onFavorite = (favorite) => { 
+      const newFavorites = [...state.myFavorites]
+      const isNewFavorite = newFavorites.some(item => item.id === favorite.id)
+
+      if(!isNewFavorite || newFavorites.length === 0) {
+          newFavorites.push(favorite)
+      }else{
+          const value = newFavorites.findIndex(item => item === favorite)
+          newFavorites.splice(value, 1)
+      } 
+
+      dispach({ type: 'NEW_FAVORITE', payload: newFavorites })
+  }
+  
   return (
     <React.Fragment>
       <Header 
         darkMode={darkMode}
         setDarkMode={setDarkMode}
       />
-      <Character 
-        isDark = {darkMode}
-      />
+
+      {state.myFavorites.map(favorite => (
+          <li key={favorite.id}>
+          {favorite.name}
+          </li>
+      ))}
+
+      <Character isDark = {darkMode}>
+        {state.characters.map(character => (
+          <CharacterItem
+            key={character.id}
+            character = {character}
+            onFavorite = {onFavorite}
+          />
+        ))}
+      </Character>
     </React.Fragment>
   );
+}
+
+
+const initialState = {
+  characters: [],
+  myFavorites: [],
+}
+
+const favoriteReducer=(state, action)=>{
+  switch (action.type){
+      case 'NEW_FAVORITE':
+          return{
+              ...state,
+              myFavorites: action.payload
+          }
+      case 'CHARACTERS':
+          return{
+              ...state,
+              characters: action.payload
+          }
+      default:
+          return { ...state }
+  }
+
 }
 
 export default App;
--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,69 +1,13 @@
-import React from 'react'
+import React, { Children } from 'react'
 import styles from './Character.module.css'
-import { CharacterItem } from './CharacterItem'
 
-function Character({isDark}) {
-    const [state, dispach] = React.useReducer(favoriteReducer, initialState)
-    
-    React.useEffect(()=>{
-        fetch('https://rickandmortyapi.com/api/character/')
-        .then(response => response.json())
-        .then(data => dispach({ type: 'CHARACTERS', payload: data.results }))
-    },[])
-
-
-    const onFavorite = (favorite) => { 
-        const newFavorites = [...state.myFavorites]
-        const isNewFavorite = newFavorites.some(item => item.id === favorite.id)
-
-        if(!isNewFavorite || newFavorites.length === 0) {
-            newFavorites.push(favorite)
-        }else{
-            const value = newFavorites.findIndex(item => item === favorite)
-            newFavorites.splice(value, 1)
-        } 
-        dispach({ type: 'NEW_FAVORITE', payload: newFavorites })
-    }
-
+function Character(props) {
   return (
-    <section className={`${styles.Characters} ${isDark && styles.dark}`}>
-        {state.myFavorites.map(favorite => (
-            <li key={favorite.id}>
-            {favorite.name}
-            </li>
-        ))}
-        {state.characters.map(character =>(
-            <CharacterItem
-                character = {character}
-                onFavorite = {onFavorite}
-                key={character.id}
-            />
-        ))}
+    <section className={`${styles.Characters} ${props.isDark && styles.dark}`}>
+       {props.children}
     </section>
   )
 }
 
-const initialState = {
-    characters: [],
-    myFavorites: [],
-}
-
-const favoriteReducer=(state, action)=>{
-    switch (action.type){
-        case 'NEW_FAVORITE':
-            return{
-                ...state,
-                myFavorites: action.payload
-            }
-        case 'CHARACTERS':
-            return{
-                ...state,
-                characters: action.payload
-            }
-        default:
-            return { ...state }
-    }
-
-}
 
 export { Character }
